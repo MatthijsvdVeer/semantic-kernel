339deb09e9a54b0c681adc61758dc7e33506ffe8
Matthijs van der Veer <736727+MatthijsvdVeer@users.noreply.github.com>
2023-11-30 08:55:00 +0100

    Adding configuration and basic notebook (#6)
    * Adding basic configuration
    
    * Finishing initial setup
    
    feat(notebook-setup): add dependency installation and AI joke generation cells
    
    - Expanded the setup notebook with instructions for installing dependencies.
    - Added code cells for building the Semantic Kernel with Azure or OpenAI service configurations.
    - Included an example to generate and display a short joke using the Semantic Kernel functionality.
    - Updated the Configuration class to initialize OpenAiSettings with the provided OpenAiProvider.
    - Ensured that the URL input is only prompted for when using Azure as the provider within the configuration setup.

---
+++ examples/config/Configuration.cs
@@ -0,0 +1,67 @@
+using InteractiveKernel = Microsoft.DotNet.Interactive.Kernel;
+using System.IO;
+using System.Text.Json;
+
+public static class Configuration
+{
+    private const string configFile = "../../examples/config/settings.json";
+
+    public static async Task<OpenAiSettings> GetSettings(OpenAiProvider openAiProvider)
+    {
+        var settings = new OpenAiSettings
+        {
+            OpenAiProvider = openAiProvider
+        };
+
+        if (File.Exists(configFile))
+        {
+            System.Console.WriteLine($"Reading settings from {configFile}.");
+            var json = File.ReadAllText(configFile);
+            settings = JsonSerializer.Deserialize<OpenAiSettings>(json);
+        }
+
+        if (string.IsNullOrEmpty(settings.ApiKey))
+        {
+            settings.ApiKey = await InteractiveKernel.GetInputAsync("API key.");
+        }
+
+        if (string.IsNullOrEmpty(settings.Deployment))
+        {
+            settings.Deployment = await InteractiveKernel.GetInputAsync("Model name.");
+        }
+
+        if (string.IsNullOrEmpty(settings.Url) && openAiProvider == OpenAiProvider.Azure)
+        {
+            settings.Url = await InteractiveKernel.GetInputAsync("URL.");
+        }
+
+        if (string.IsNullOrEmpty(settings.Organisation) && openAiProvider == OpenAiProvider.OpenAi)
+        {
+            settings.Organisation = await InteractiveKernel.GetInputAsync("Organisation.");
+        }
+
+        var text = JsonSerializer.Serialize(settings, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(configFile, text);
+
+        return settings;
+    }
+}
+
+public sealed class OpenAiSettings
+{
+    public string ApiKey { get; set; }
+
+    public string Deployment { get; set; }
+
+    public string Url { get; set; }
+
+    public string Organisation { get; set; }
+
+    public OpenAiProvider OpenAiProvider { get; set; }
+}
+
+public enum OpenAiProvider
+{
+    Azure,
+    OpenAi
+}
